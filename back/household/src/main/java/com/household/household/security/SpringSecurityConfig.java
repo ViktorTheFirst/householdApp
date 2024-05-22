4db9dab89e02a515ece979888311b254bccd4061
Roman Kushner <73998196+KushnerRoman@users.noreply.github.com>
2024-05-22 16:36:13 +0300

    registration,login,exception handler, token response,security config

--- back/household/src/main/java/com/household/household/security/SpringSecurityConfig.java
+++ back/household/src/main/java/com/household/household/security/SpringSecurityConfig.java
@@ -6,27 +6,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+
 public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception
-    { http
-            .authorizeHttpRequests(authReq->authReq
-                    .requestMatchers("/**").permitAll()).formLogin(Customizer.withDefaults()).httpBasic().disable();
+    {
+
+       return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth-> auth.requestMatchers("/api/v1/auth/*")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                ).build();
 
 
 
 
 
 
-        return  http.build();
+
     }
+
 
 
 }
